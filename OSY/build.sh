1ade4d761863c9a0da1bf1086635d1703cb3c843
JosiahShanti <94713473+JosiahShanti@users.noreply.github.com>
2022-02-12 13:58:50 -0600

    Delete OSY directory

--- OSY/build.sh
+++
@@ -1,9 +0,0 @@
-rm main.bin
-rm main.lnkd
-
-echo "start"
-python linker.py main.asm 
-
-nasm main.lnkd -o main.bin
-
-kvm main.bin
--- OSY/console.asm
+++
@@ -1,8 +0,0 @@
-.data
-prompt db "->", 0
-.code
-
-cons:
-	nop
-.loop:
-	jmp .loop
