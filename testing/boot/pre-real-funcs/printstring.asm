182d3f7041d76a8159b6d0c26000aa7572134ca3
JosiahShanti <94713473+JosiahShanti@users.noreply.github.com>
2022-02-14 15:31:19 -0600

    Update printstring.asm

--- testing/boot/pre-real-funcs/printstring.asm
+++ testing/boot/pre-real-funcs/printstring.asm
@@ -1,2 +1,28 @@
+; nsbu = (not set by user)
+
+; print string function for 16 bit real mode
 16pstr:
-  nop
+  ; ax- adress of the string
+  ; bx-(nsbu)-
+  ; save reg
+  push ax
+  push bx
+  
+  ;get char at bx into ax 
+  mov bx, ax
+  mov ax, [bx]
+  
+.loop:
+  ; call print char
+  call 16pchar
+  ; get char at bx into ax 
+  inc bx
+  mov ax, [bx]
+  cmp ax, '$'
+  
+  ; bring back reg
+  pop ax
+  pop bx 
+  
+  ;return
+  ret
